e4359e60c48229d583fd7df1f9ad586b5437ca01
Jason Johnston <github@lojjic.net>
2006-12-27 03:38:15 +0000

    Attempt to make DOMWrapFactory more flexible so wrapper class mappings are added dynamically rather than hardcoded.
    git-svn-id: https://svn.lojjic.net/xul/trunk@533 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

--- dom-core-javascript/src/main/java/net/lojjic/xml/javascript/DOMWrapFactory.java
+++ dom-core-javascript/src/main/java/net/lojjic/xml/javascript/DOMWrapFactory.java
@@ -3,21 +3,25 @@ package net.lojjic.xml.javascript;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.WrapFactory;
-import org.w3c.dom.Attr;
-import org.w3c.dom.CharacterData;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.lang.reflect.Constructor;
 
 public class DOMWrapFactory extends WrapFactory {
 	
-	
-	
-	public void add(Class domClass, Class scriptableClass) {
-		
+	private HashMap<Class, Class<? extends ScriptableDOMObject>> mappings = new HashMap<Class, Class<? extends ScriptableDOMObject>>();
+
+
+	/**
+	 * Add a mapping from a DOM class/interface to its Scriptable wrapper class.
+	 * @param domClass The DOM class or interface to be wrapped
+	 * @param scriptableClass The Scriptable class in which instances of the DOM class
+	 *        should be wrapped. Must be a subclass of {@link ScriptableDOMObject}.
+	 */
+	public void addWrapMapping(Class domClass, Class<? extends ScriptableDOMObject> scriptableClass) {
+		mappings.put(domClass, scriptableClass);
 	}
+
 
 	/**
 	 * Override the base functionality to handle our special DOM object
@@ -32,33 +36,48 @@ public class DOMWrapFactory extends WrapFactory {
 	 */
 	@Override
 	public Scriptable wrapAsJavaObject(Context cx, Scriptable scope, Object javaObject, Class staticType) {
-		
-		//if(javaObject instanceof XULElement) {
-		//	return new ScriptableXULElement(scope, (XULElement)javaObject);
-		//}
-		if(javaObject instanceof Element) {
-			return new ScriptableElement(scope, (Element)javaObject);
+
+		// If a matching wrapper class is defined in the mappings, create an instance:
+		Class javaClass = javaObject.getClass();
+		Class wrapper = getWrapperForClass(javaClass);
+		if(wrapper != null) {
+			try {
+				Constructor constructor = wrapper.getConstructor(Scriptable.class, javaClass);
+				return (Scriptable)constructor.newInstance(scope, javaObject);
+			}
+			catch(Exception e) {
+				// TODO handle more elegantly
+				System.out.println("Problem instantiating JavaScript wrapper class; falling back to default wrapper.");
+			}
 		}
-		if(javaObject instanceof Text) {
-			return new ScriptableText(scope, (Text)javaObject);
-		}
-		if(javaObject instanceof CharacterData) {
-			return new ScriptableCharacterData(scope, (CharacterData)javaObject);
-		}
-		if(javaObject instanceof Attr) {
-			return new ScriptableAttr(scope, (Attr)javaObject);
-		}
-		if(javaObject instanceof Document) {
-			return new ScriptableDocument(scope, (Document)javaObject);
-		}
-		if(javaObject instanceof Node) {
-			return new ScriptableNode(scope, (Node)javaObject);
-		}
-		if(javaObject instanceof NodeList) {
-			return new ScriptableNodeList(scope, (NodeList)javaObject);
-		}
-		
+
 		return super.wrapAsJavaObject(cx, scope, javaObject, staticType);
+	}
+
+
+	/**
+	 * Find the most specific mapped wrapper for the given Class.
+	 * Walks the inheritance line, checking each class and its explicitly
+	 * declared interfaces for a match in the mappings. If no mapping
+	 * is found, null is returned.
+	 */
+	private Class getWrapperForClass(Class clazz) {
+		while(clazz != null) {
+			// Try the class directly:
+			Class wrapper = mappings.get(clazz);
+			if(wrapper != null) {
+				return wrapper;
+			}
+			// Try the class's interfaces:
+			for(Class iface : clazz.getInterfaces()) {
+				wrapper = mappings.get(iface);
+				if(wrapper != null) {
+					return wrapper;
+				}
+			}
+			clazz = clazz.getSuperclass();
+		}
+		return null;
 	}
 
 }
