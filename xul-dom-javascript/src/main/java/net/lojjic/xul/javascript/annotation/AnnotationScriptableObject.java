38ab4ad9d09827e4b6bbf98ce4ee7ea8f3affcb2
Jason Johnston <github@lojjic.net>
2007-01-04 06:05:12 +0000

    factor chunks out into separate methods
    git-svn-id: https://svn.lojjic.net/xul/trunk@553 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

--- xul-dom-javascript/src/main/java/net/lojjic/xul/javascript/annotation/AnnotationScriptableObject.java
+++ xul-dom-javascript/src/main/java/net/lojjic/xul/javascript/annotation/AnnotationScriptableObject.java
@@ -7,7 +7,6 @@ import org.mozilla.javascript.ScriptRuntime;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +14,16 @@ import java.util.Map;
  *
  */
 public abstract class AnnotationScriptableObject extends ScriptableObject {
+
+	/**
+	 * Data container for categorized annotated methods
+	 */
+	protected static class AnnotatedMethods {
+		Method constructor = null;
+		Map<String,Method> propertyGetters = new HashMap<String,Method>();
+		Map<String,Method> propertySetters = new HashMap<String,Method>();
+		Map<String,Method> functions = new HashMap<String,Method>();
+	}
 
 	public static void defineClass(Scriptable scope, Class clazz)
 			throws IllegalAccessException, InstantiationException, InvocationTargetException {
@@ -32,9 +41,86 @@ public abstract class AnnotationScriptableObject extends ScriptableObject {
 		// Create the prototype instance:
 		Scriptable prototype = (Scriptable)clazz.newInstance();
 
+		// Set the prototype's prototype:
+		prototype.setPrototype(getSuperPrototype(scope, clazz, sealed, mapInheritance));
 
-		// Set the prototype's prototype, trying to map Java inheritance to JS
-		// prototype-based inheritance if requested to do so.
+		// Get the JS class name:
+		String className = getJSClassName(clazz);
+
+
+		// Collect the class's annotated methods:
+		AnnotatedMethods methods = getAnnotatedMethods(clazz);
+
+		// Add the constructor and prototype to the parent scope:
+		if(methods.constructor != null) {
+			FunctionObject ctor = new FunctionObject(className, methods.constructor, scope);
+			ctor.addAsConstructor(scope, prototype);
+		}
+
+		// Loop over the getters and add them and any corresponding setters
+		// to the prototype:
+		for(Map.Entry<String,Method> entry : methods.propertyGetters.entrySet()) {
+			Method setter = methods.propertySetters.get(entry.getKey());
+			int attr = PERMANENT | DONTENUM | (setter != null ? 0 : READONLY);
+			((ScriptableObject)prototype).defineProperty(entry.getKey(), null, entry.getValue(), setter, attr);
+		}
+
+		// Add the functions to the prototype:
+		for(Map.Entry<String,Method> entry : methods.functions.entrySet()) {
+			FunctionObject func = new FunctionObject(entry.getKey(), entry.getValue(), prototype);
+			((ScriptableObject)prototype).defineProperty(entry.getKey(), func, DONTENUM);
+		}
+
+		return className;
+	}
+
+
+	/**
+	 * Get the target JavaScript class name for the given Class.
+	 * Uses the {@link net.lojjic.xul.javascript.annotation.JSClassName}
+	 * annotation if available, otherwise uses the Class's unqualified name.
+	 */
+	protected static String getJSClassName(Class clazz) {
+		return (clazz.isAnnotationPresent(JSClassName.class))
+				? ((JSClassName)clazz.getAnnotation(JSClassName.class)).value()
+				: clazz.getSimpleName();
+	}
+
+
+	/**
+	 * Collect the annotated methods for the given Class.
+	 */
+	protected static AnnotatedMethods getAnnotatedMethods(Class clazz) {
+		AnnotatedMethods result = new AnnotatedMethods();
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			// Constructor - only look for one:
+			if(result.constructor == null && method.isAnnotationPresent(JSConstructor.class)) {
+				result.constructor = method;
+			}
+			// Property getters:
+			if(method.isAnnotationPresent(JSGetter.class)) {
+				result.propertyGetters.put(method.getAnnotation(JSGetter.class).value(), method);
+			}
+			// Property setters:
+			if(method.isAnnotationPresent(JSSetter.class)) {
+				result.propertySetters.put(method.getAnnotation(JSSetter.class).value(), method);
+			}
+			// Functions:
+			if(method.isAnnotationPresent(JSFunction.class)) {
+				result.functions.put(method.getAnnotation(JSFunction.class).value(), method);
+			}
+		}
+		return result;
+	}
+
+
+	/**
+	 * Build the prototype's prototype, trying to map Java inheritance to JS
+	 * prototype-based inheritance if requested to do so.
+	 */
+	protected static Scriptable getSuperPrototype(Scriptable scope, Class clazz, boolean mapInheritance, boolean sealed)
+			throws IllegalAccessException, InstantiationException, InvocationTargetException {
 		Scriptable superProto = null;
 		if (mapInheritance) {
 			Class superClass = clazz.getSuperclass();
@@ -48,65 +134,9 @@ public abstract class AnnotationScriptableObject extends ScriptableObject {
 		if (superProto == null) {
 			superProto = ScriptableObject.getObjectPrototype(scope);
 		}
-		prototype.setPrototype(superProto);
-
-
-		// Get the JS class name:
-		String className = (clazz.isAnnotationPresent(JSClassName.class))
-				? ((JSClassName)clazz.getAnnotation(JSClassName.class)).value()
-				: clazz.getSimpleName();
-
-
-		// Go through the class's methods and collect the annotated ones:
-		Method constructor = null;
-		HashMap<String,Method> getters = new HashMap<String,Method>();
-		HashMap<String,Method> setters = new HashMap<String,Method>();
-		HashMap<String,Method> functions = new HashMap<String,Method>();
-
-		Method[] methods = clazz.getDeclaredMethods();
-		for (Method method : methods) {
-			// Constructor - only look for one:
-			if(constructor == null && method.isAnnotationPresent(JSConstructor.class)) {
-				constructor = method;
-			}
-
-			// Property getters:
-			if(method.isAnnotationPresent(JSGetter.class)) {
-				getters.put(method.getAnnotation(JSGetter.class).value(), method);
-			}
-
-			// Property setters:
-			if(method.isAnnotationPresent(JSSetter.class)) {
-				setters.put(method.getAnnotation(JSSetter.class).value(), method);
-			}
-
-			// Functions:
-			if(method.isAnnotationPresent(JSFunction.class)) {
-				functions.put(method.getAnnotation(JSFunction.class).value(), method);
-			}
-		}
-
-		// Add the constructor and prototype to the parent scope:
-		if(constructor != null) {
-			FunctionObject ctor = new FunctionObject(className, constructor, scope);
-			ctor.addAsConstructor(scope, prototype);
-		}
-
-		// Loop over the getters and add them and any corresponding setters
-		// to the prototype:
-		for(Map.Entry<String,Method> entry : getters.entrySet()) {
-			Method setter = setters.get(entry.getKey());
-			int attr = PERMANENT | DONTENUM | (setter != null ? 0 : READONLY);
-			((ScriptableObject)prototype).defineProperty(entry.getKey(), null, entry.getValue(), setter, attr);
-		}
-
-		// Add the functions to the prototype:
-		for(Map.Entry<String,Method> entry : functions.entrySet()) {
-			FunctionObject func = new FunctionObject(entry.getKey(), entry.getValue(), prototype);
-			((ScriptableObject)prototype).defineProperty(entry.getKey(), func, DONTENUM);
-		}
-
-		return className;
+		return superProto;
 	}
+
+
 
 }
