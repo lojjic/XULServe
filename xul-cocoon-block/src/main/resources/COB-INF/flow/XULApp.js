2324d5bddf03d2202e663bb64877fe3f593171e2
Jason Johnston <github@lojjic.net>
2007-01-13 18:15:49 +0000

    add skeleton for handling form field values and events from client
    git-svn-id: https://svn.lojjic.net/xul/trunk@582 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

--- xul-cocoon-block/src/main/resources/COB-INF/flow/XULApp.js
+++ xul-cocoon-block/src/main/resources/COB-INF/flow/XULApp.js
@@ -2,8 +2,8 @@
 
 
 
-function XULApp() {
-	this.app = new Packages.net.lojjic.xul.XULApp(cocoon.parameters.file);
+function XULApp(file) {
+	this.window = new Packages.net.lojjic.xul.impl.WindowImpl(TODO);
 }
 
 XULApp.prototype = {
@@ -15,17 +15,40 @@ XULApp.prototype = {
 		var bookmark = cocoon.createWebContinuation();
 
 		if(redisplaying) {
+			// Update form widget values:
+			for(var names = cocoon.request.getParameterNames(); names.hasMoreElements();) {
+				var name = names.nextElement();
+				if(name.match(/^xul.field./)) {
+					var field = this.window.document.getElementById(name.replace(/^xul.field./, ""));
+					if(field) {
+						field.value = cocoon.request.getParameter(name);
+					}
+				}
+			}
+
+			// Dispatch event:
 			var eventType = cocoon.request.getParameter("xul.event.type");
 			var eventTarget = cocoon.request.getParameter("xul.event.target");
 			if(eventType && eventTarget) {
-				this.app.handleEvent(eventType, eventTarget);
+				var tgt = this.window.document.getElementById(eventTarget);
+				if(tgt) {
+					var evt = this.window.document.createEvent("UIEvents"); //TODO handle other event families
+					evt.initUIEvent(eventType, true, true, this.window, cocoon.request.getParameter("xul.event.detail"));
+					tgt.dispatchEvent(evt);
+				}
 			}
 		}
 
 		redisplaying = true;
 
-		cocoon.sendPage(pipeline, {xul:this.app}, bookmark);
+		cocoon.sendPage(pipeline, {xul:TODO}, bookmark);
 
 	}
 
+};
+
+XULApp.start = function() {
+	var file = cocoon.parameters.file;
+	var app = new XULApp(file);
+	app.display();
 };
