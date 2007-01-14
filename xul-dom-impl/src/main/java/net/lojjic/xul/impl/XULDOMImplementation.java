67596419709cc6221b3547f22947c7d37c6e0c09
Jason Johnston <github@lojjic.net>
2007-01-14 20:27:01 +0000

    add XUL DOMImplementation
    git-svn-id: https://svn.lojjic.net/xul/trunk@590 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

---
+++ xul-dom-impl/src/main/java/net/lojjic/xul/impl/XULDOMImplementation.java
@@ -0,0 +1,40 @@
+package net.lojjic.xul.impl;
+
+import org.apache.xerces.dom.DOMImplementationImpl;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentType;
+
+/**
+ * DOMImplementation for XUL documents
+ */
+public class XULDOMImplementation extends DOMImplementationImpl implements DOMImplementation {
+
+	/**
+	 * Test if the DOM implementation implements a specific feature and
+	 * version, as specified in .
+	 *
+	 * @param feature The name of the feature to test.
+	 * @param version This is the version number of the feature to test.
+	 * @return <code>true</code> if the feature is implemented in the
+	 *         specified version, <code>false</code> otherwise.
+	 */
+	public boolean hasFeature(String feature, String version) {
+		// TODO handle any additional features
+		return super.hasFeature(feature, version);
+	}
+
+	/**
+	 * Creates a DOM Document object of the specified type with its document
+	 * element.
+	 * @see org.w3c.dom.DOMImplementation#createDocument(String, String, org.w3c.dom.DocumentType)
+	 */
+	public Document createDocument(String namespaceURI, String qualifiedName, DocumentType doctype)
+			throws DOMException {
+		Document doc = new XULDocumentImpl();
+		doc.appendChild(doc.createElementNS(namespaceURI, qualifiedName));
+		return doc;
+	}
+
+}
