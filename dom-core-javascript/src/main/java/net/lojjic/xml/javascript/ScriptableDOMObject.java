eb3f1fe8023193f1be30b263e22e38b52f8664a7
Jason Johnston <github@lojjic.net>
2007-02-13 01:58:05 +0000

    cache the scriptable wrapper as user data on the node, so the same instance is always returned
    git-svn-id: https://svn.lojjic.net/xul/trunk@607 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

--- dom-core-javascript/src/main/java/net/lojjic/xml/javascript/ScriptableDOMObject.java
+++ dom-core-javascript/src/main/java/net/lojjic/xml/javascript/ScriptableDOMObject.java
@@ -3,8 +3,11 @@ package net.lojjic.xml.javascript;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
+import org.w3c.dom.Node;
 
 public abstract class ScriptableDOMObject extends ScriptableObject {
+
+	public static final String USER_DATA_KEY = "ScriptableDOMObject:UserDataKey";
 
 	public static String JS_CLASS_NAME;
 	
@@ -19,7 +22,17 @@ public abstract class ScriptableDOMObject extends ScriptableObject {
 
 
 	protected Object wrap(Object javaObject) {
-		return Context.javaToJS(javaObject, getTopLevelScope(this));
+		Object wrapper = null;
+		if(javaObject instanceof Node) {
+			wrapper = ((Node)javaObject).getUserData(USER_DATA_KEY);
+		}
+		if(wrapper == null) {
+			wrapper = Context.javaToJS(javaObject, getTopLevelScope(this));
+			if(javaObject instanceof Node) {
+				((Node)javaObject).setUserData(USER_DATA_KEY, wrapper, null);
+			}
+		}
+		return wrapper;
 	}
 
 
