2859ea57979f19194def3ff53373b6389b9ee768
Jason Johnston <github@lojjic.net>
2007-04-16 00:47:23 +0000

    documentation
    git-svn-id: https://svn.lojjic.net/xul/trunk@689 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

--- xul-rdf-impl/src/main/java/net/lojjic/xul/rdf/impl/SesameConnectionCallback.java
+++ xul-rdf-impl/src/main/java/net/lojjic/xul/rdf/impl/SesameConnectionCallback.java
@@ -4,6 +4,10 @@ import org.openrdf.repository.RepositoryConnection;
 
 /**
  * Callback interface for executing code within a Sesame repository connection.
+ * Implement this interface and pass the result as an argument to
+ * {@link net.lojjic.xul.rdf.impl.SesameUtils#execute(org.openrdf.repository.Repository, SesameConnectionCallback)}
+ * to execute Sesame repository operations without having to worry about connection
+ * setup and cleanup.
  */
 interface SesameConnectionCallback<T> {
 
