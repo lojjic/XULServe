3728bebc9670ce9be1c451bf21018ea9ff844f00
Jason Johnston <github@lojjic.net>
2007-05-12 06:37:37 +0000

    Add class for handling template rule bindings
    git-svn-id: https://svn.lojjic.net/xul/trunk@721 8f8d9d7b-adf4-0310-9a85-ad1d9f542d00

---
+++ xul-dom-impl/src/main/java/net/lojjic/xul/impl/templates/Binding.java
@@ -0,0 +1,35 @@
+package net.lojjic.xul.impl.templates;
+
+import net.lojjic.xul.rdf.RDFService;
+import net.lojjic.xul.rdf.RDFDataSource;
+import net.lojjic.xul.rdf.RDFNode;
+import net.lojjic.xul.rdf.RDFResource;
+
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Template rule binding. Very similar to a triple condition except that it does not
+ * remove variable sets that don't match the triple.
+ */
+public class Binding extends TripleCondition {
+
+	public Binding(RDFService rdfService, String subject, String predicate, String object) {
+		super(rdfService, subject, predicate, object);
+	}
+
+	@Override
+	public void applyToVariablesList(RDFDataSource dataSource, List<Map<String, RDFNode>> varsList, RDFResource start) {
+		ArrayList<Map<String, RDFNode>> varsListCopy = new ArrayList<Map<String, RDFNode>>(varsList);
+		for(int i = 0; i < varsListCopy.size(); i++) {
+			Map<String, RDFNode> vars = varsListCopy.get(i);
+			List<Map<String, RDFNode>> newVarsList = applyToVars(dataSource, vars);
+			if(newVarsList != null) {
+				varsList.remove(i);
+				varsList.addAll(i, newVarsList);
+			}
+		}
+	}
+
+}
--- xul-dom-impl/src/main/java/net/lojjic/xul/impl/templates/TripleCondition.java
+++ xul-dom-impl/src/main/java/net/lojjic/xul/impl/templates/TripleCondition.java
@@ -27,44 +27,57 @@ public class TripleCondition extends Condition {
 	}
 
 	public void applyToVariablesList(RDFDataSource dataSource, List<Map<String, RDFNode>> varsList, RDFResource start) {
-		for(Map<String, RDFNode> vars : new ArrayList<Map<String, RDFNode>>(varsList)) {
-			if(subjectAttr.isVariable() && vars.containsKey(subjectAttr.getVarName())) {
-				RDFNode varValue = vars.get(subjectAttr.getVarName());
-				if(varValue instanceof RDFResource) {
-					if(objectAttr.isVariable()) {
-						Iterator<RDFNode> targets = dataSource.getTargets((RDFResource)varValue, predicateAttr.getRDFResource(), true);
-						while(targets.hasNext()) {
-							Map<String, RDFNode> newVars = new HashMap<String, RDFNode>(vars);
-							newVars.put(objectAttr.getVarName(), targets.next());
-							varsList.add(newVars);
-						}
-						varsList.remove(vars);
-					}
-					else if(!((objectAttr.isURI() && dataSource.hasAssertion((RDFResource)varValue, predicateAttr.getRDFResource(), objectAttr.getRDFResource(), true))
-							|| dataSource.hasAssertion((RDFResource)varValue, predicateAttr.getRDFResource(), objectAttr.getRDFLiteral(), true))) {
-						varsList.remove(vars);
-					}
-				} else {
-					varsList.remove(vars);
-				}
-			}
-			else if(objectAttr.isVariable() && vars.containsKey(objectAttr.getVarName())) {
-				RDFNode varValue = vars.get(objectAttr.getVarName());
-				if(subjectAttr.isVariable()) {
-					Iterator<RDFResource> sources = dataSource.getSources(predicateAttr.getRDFResource(), varValue, true);
-					while(sources.hasNext()) {
-						Map<String, RDFNode> newVars = new HashMap<String, RDFNode>(vars);
-						newVars.put(subjectAttr.getVarName(), sources.next());
-						varsList.add(newVars);
-					}
-					varsList.remove(vars);
-				} else if(!dataSource.hasAssertion(subjectAttr.getRDFResource(), predicateAttr.getRDFResource(), varValue, true)) {
-					varsList.remove(vars);
-				}
-			} else {
-				varsList.remove(vars);
+		ArrayList<Map<String, RDFNode>> varsListCopy = new ArrayList<Map<String, RDFNode>>(varsList);
+		for(int i = 0; i < varsListCopy.size(); i++) {
+			Map<String, RDFNode> vars = varsListCopy.get(i);
+			varsList.remove(i);
+			List<Map<String, RDFNode>> newVarsList = applyToVars(dataSource, vars);
+			if(newVarsList != null) {
+				varsList.addAll(i, newVarsList);
 			}
 		}
-
 	}
+
+	protected List<Map<String, RDFNode>> applyToVars(RDFDataSource dataSource, Map<String, RDFNode> vars) {
+		if(subjectAttr.isVariable() && vars.containsKey(subjectAttr.getVarName())) {
+			RDFNode varValue = vars.get(subjectAttr.getVarName());
+			if(varValue instanceof RDFResource) {
+				if(objectAttr.isVariable()) {
+					List<Map<String, RDFNode>> newVarsList = new ArrayList<Map<String, RDFNode>>();
+					Iterator<RDFNode> targets = dataSource.getTargets((RDFResource)varValue, predicateAttr.getRDFResource(), true);
+					while(targets.hasNext()) {
+						Map<String, RDFNode> newVars = new HashMap<String, RDFNode>(vars);
+						newVars.put(objectAttr.getVarName(), targets.next());
+						newVarsList.add(newVars);
+					}
+					return newVarsList;
+				}
+				else if((objectAttr.isURI() && dataSource.hasAssertion((RDFResource)varValue, predicateAttr.getRDFResource(), objectAttr.getRDFResource(), true))
+						|| dataSource.hasAssertion((RDFResource)varValue, predicateAttr.getRDFResource(), objectAttr.getRDFLiteral(), true)) {
+					List<Map<String, RDFNode>> newVarsList = new ArrayList<Map<String, RDFNode>>();
+					newVarsList.add(vars);
+					return newVarsList;
+				}
+			}
+		}
+		else if(objectAttr.isVariable() && vars.containsKey(objectAttr.getVarName())) {
+			RDFNode varValue = vars.get(objectAttr.getVarName());
+			if(subjectAttr.isVariable()) {
+				List<Map<String, RDFNode>> newVarsList = new ArrayList<Map<String, RDFNode>>();
+				Iterator<RDFResource> sources = dataSource.getSources(predicateAttr.getRDFResource(), varValue, true);
+				while(sources.hasNext()) {
+					Map<String, RDFNode> newVars = new HashMap<String, RDFNode>(vars);
+					newVars.put(subjectAttr.getVarName(), sources.next());
+					newVarsList.add(newVars);
+				}
+				return newVarsList;
+			} else if(dataSource.hasAssertion(subjectAttr.getRDFResource(), predicateAttr.getRDFResource(), varValue, true)) {
+				List<Map<String, RDFNode>> newVarsList = new ArrayList<Map<String, RDFNode>>();
+				newVarsList.add(vars);
+				return newVarsList;
+			}
+		}
+		return null;
+	}
+
 }
